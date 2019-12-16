dad31c82691fa0e2055d6b2365076181091868b5
ninjasul <ninjasul@gmail.com>
2019-12-17 00:48:32 +0900

    mixin을 통한 반복코드 제거

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import './App.scss';
 import Button from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="buttons">
-        <Button>Button</Button>
-        <Button size={'large'}>Button</Button>
-        <Button size={'small'}>Button</Button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="buttons">
+                <Button size={'large'}>Button</Button>
+                <Button>Button</Button>
+                <Button size={'small'}>Button</Button>
+            </div>
+            <div className="buttons">
+                <Button color="gray" size={'large'}>Button</Button>
+                <Button color="gray">Button</Button>
+                <Button color="gray" size={'small'}>Button</Button>
+            </div>
+            <div className="buttons">
+                <Button color="pink" size={'large'}>Button</Button>
+                <Button color="pink">Button</Button>
+                <Button color="pink" size={'small'}>Button</Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,12 @@ import React from 'react';
 import classNames from 'classnames';
 import './Button.scss';
 
-// large, medium, small
-function Button({ children, size }) {
+// size: large, medium, small
+// color: blue, pink, gray
+function Button({ children, size, color }) {
   //return <button className={['Button', size].join(' ')}>{children}</button>;
   //return <button className={`Button ${size}`}>{children}</button>;
-  return <button className={classNames('Button', size)}>{children}</button>;
+  return <button className={classNames('Button', size, color)}>{children}</button>;
 }
 
 Button.defaultProps = {
